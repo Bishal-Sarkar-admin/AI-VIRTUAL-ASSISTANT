c30290cbda7c589f8e8d90c4271f6e2b93da5ede
Bishal Sarkar <bishalsarkar450up@gmail.com>
2024-09-26 19:29:35 +0530

    bag fix speak(text)

--- default.js
+++ default.js
@@ -19,12 +19,30 @@ window.addEventListener("load", () => {
 
 //speak function
 function speak(text) {
-  let text_speak = new SpeechSynthesisUtterance(text);
-  text_speak.rate = 1;
-  text_speak.pitch = 1;
-  text_speak.volume = 1;
-  text_speak.lang = "en-GB";
-  window.speechSynthesis.speak(text_speak);
-}
+  if ("speechSynthesis" in window) {
+    window.speechSynthesis.cancel(); // Clears the queue
+    let text_speak = new SpeechSynthesisUtterance(text);
 
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+    text_speak.volume = 1;
+    text_speak.lang = "en-GB";
+
+    text_speak.onstart = () => {
+      console.log("Speech started");
+    };
+
+    text_speak.onend = () => {
+      console.log("Speech finished");
+    };
+
+    text_speak.onerror = (e) => {
+      console.error("Speech synthesis error:", e);
+    };
+
+    window.speechSynthesis.speak(text_speak);
+  } else {
+    console.error("SpeechSynthesis API is not supported in this browser.");
+  }
+}
 //
